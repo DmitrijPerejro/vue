417bb2a4ac7f29a6812ee89db36b65cff9f4d060
Perejro <krulkevych@gmail.com>
2018-07-05 10:43:55 +0300

    events

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ new Vue({
    data: {
        title: {
            '1': 'DynamicAttr',
-           '2': 'add HTML'
+           '2': 'add HTML',
+           '3': 'Events'
        },
        url: 'http://vuejs.org',
-       myHTML: '<div><p>Hello, I m from script</p></div>'
-   }
+       myHTML: '<div><p>Hello, I m from script</p></div>',
+       counter: 0
+   },
+    methods: {
+       addCount(){
+           this.counter++;
+       },
+       hover(event) {
+           event.target.style.outline = '2px solid blue'
+       },
+       unHover(event) {
+           event.target.style.outline = ''
+       }
+    }
 });
